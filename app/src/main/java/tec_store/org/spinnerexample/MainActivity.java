8c6803e385d74c60b69faaee483966e814788739
tecstoreorg <allomi_95@hotmail.com>
2016-12-17 18:14:42 -0800

    Tec-Store.org :D

---
+++ app/src/main/java/tec_store/org/spinnerexample/MainActivity.java
@@ -0,0 +1,51 @@
+package tec_store.org.spinnerexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        List<String> spinnerList = new ArrayList<>();
+        spinnerList.add("Google");
+        spinnerList.add("Microsoft");
+        spinnerList.add("Apple");
+        spinnerList.add("Dell");
+        spinnerList.add("HP");
+        spinnerList.add("ASUS");
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.spinner_row,spinnerList);
+
+
+        spinner.setAdapter(adapter);
+
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "you clicked "+adapterView.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+}
